bae9fdc6e11a31b84766d07eab8bc8fa2a0380c0
Mav Sanchez <maverickssanchez@gmail.com>
2020-02-13 04:01:01 -0800

    Finished part 3

--- app.py
+++ app.py
@@ -7,6 +7,10 @@ from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
 from sqlalchemy import create_engine, func
 from flask import Flask, jsonify, render_template
+from sqlalchemy import or_
+from sqlalchemy import and_
+from dateutil.relativedelta import *
+
 # start database setup
 
 engine = create_engine("sqlite:///Resources/hawaii.sqlite")
@@ -41,23 +45,47 @@ def precipitation():
     # python_data = json.loads(json.dumps(all_measurements))
     return render_template('index.html', json_content=all_measurements)
 
-    # all_names = list(np.ravel(results))
-    # return jsonify(all_names)
+@app.route("/api/v1.0/stations", methods=['GET', 'POST'])
+def stations():
+    session = Session(engine)
+    results = session.query(Station.id, Station.station, Station.name, Station.latitude, Station.longitude, Station.elevation).all()
+    session.close()
+    all_stations = []
+    # format into JSON
+    for id, station, name, latitude, longitude, elevation in results:
+        station_dict = {}
+        station_dict['ID'] = id
+        station_dict['Station'] = station
+        station_dict['Name'] = name
+        station_dict['Latitude'] = latitude
+        station_dict['Longitude'] = longitude
+        station_dict['Elevation'] = elevation
+        all_stations.append(station_dict)
+    # python_data = json.loads(json.dumps(all_measurements))
+    return render_template('index.html', json_content=all_stations)
 
-# @app.route("/api/v1.0/measurements")
-# def measurements():
-#     session = Session(engine)
-#     results = session.query(Measurement.name, Measurement.age, Measurement.sex).all()
-#     session.close()
-#     all_measurements = []
-#     # format into JSON
-#     for name, age, sex in results:
-#         measurement_dict = {}
-#         measurement_dict['name'] = name
-#         measurement_dict['age'] = age
-#         measurement_dict['sex'] = sex
-#         all_measurements.append(measurement_dict)
-#     return jsonify(all_measurements)
+
+@app.route("/api/v1.0/tobs", methods=['GET', 'POST'])
+def tobs():
+    session = Session(engine)
+    max_date = session.query(func.max(Measurement.date)).first()
+    maxdate = dt.datetime.strptime(str(max_date[0]), "%Y-%m-%d")
+    #Use relative data to get the last 12 months from the date of the max date
+    last_12_months = maxdate + relativedelta(months=-12)
+    results = session.query(Measurement.date, Measurement.tobs).filter(and_(Measurement.date>=last_12_months, Measurement.date <= maxdate)).all()
+    # results = session.query(Measurement.date, Measurement.tobs).limit(10).all()
+    session.close()
+    all_measurements = []
+    # format into JSON
+    for date, tobs in results:
+        measurement_dict = {}
+        measurement_dict['Date'] = date
+        measurement_dict['Precipitation'] = tobs
+        all_measurements.append(measurement_dict)
+    
+    # python_data = json.loads(json.dumps(all_measurements))
+    return render_template('index.html', json_content=all_measurements)
+
 
 if __name__ == "__main__":
     app.run(debug=True)

c456b69634e017a365cd0f0b46ad2e239f5d9c2c
Mav Sanchez <maverickssanchez@gmail.com>
2020-02-14 05:41:43 -0800

    Submitting final work

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
 from sqlalchemy import create_engine, func
 from flask import Flask, jsonify, render_template
+from flask import abort
 from sqlalchemy import or_
 from sqlalchemy import and_
 from dateutil.relativedelta import *
@@ -28,10 +29,19 @@ app = Flask(__name__)
 def index():
     return render_template('index.html',json_content={'Welcome!': 'Contents will be displayed here'}, message_type="Success", message_content="Welcome!")
 
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('index.html',json_content={ 'Error 404!' : 'Page not found!'}, message_type="Error", message_content="WHERE WERE YOU GOING? :/"), 404
+
+@app.errorhandler(400)
+def invalid_parameter(e):
+    return render_template('index.html',json_content={ 'Error!' : 'Invalid parameter!'}, message_type="Error", message_content="Invalid parameter passed!"), 400
+
+
 @app.route("/api/v1.0/precipitation/<type>", methods=['GET', 'POST'])
 def precipitation(type):
     session = Session(engine)
-    results = session.query(Measurement.date, Measurement.prcp).limit(10).all()
+    results = session.query(Measurement.date, Measurement.prcp).all()
     session.close()
     all_measurements = []
     # format into JSON
@@ -40,11 +50,13 @@ def precipitation(type):
         measurement_dict['Date'] = date
         measurement_dict['Precipitation'] = prcp
         all_measurements.append(measurement_dict)
-        
-    if type != "raw":
+
+    if type == "inline":
         return render_template('index.html', json_content=all_measurements, message_type="Success", message_content="Success! Returning Precipitation data")
-    else:
+    elif type == "raw":
         return jsonify(all_measurements)
+    else:
+        abort(404)
 
 @app.route("/api/v1.0/stations/<type>", methods=['GET', 'POST'])
 def stations(type):
@@ -63,11 +75,12 @@ def stations(type):
         station_dict['Elevation'] = elevation
         all_stations.append(station_dict)
     
-    if type != "raw":
+    if type == "inline":
         return render_template('index.html', json_content=all_stations, message_type="Success", message_content="Success! Returning Stations data")
-    else:
+    elif type == "raw":
         return jsonify(all_stations)
-
+    else:
+        abort(404)
 
 @app.route("/api/v1.0/tobs/<type>", methods=['GET', 'POST'])
 def tobs(type):
@@ -77,7 +90,6 @@ def tobs(type):
     #Use relative data to get the last 12 months from the date of the max date
     last_12_months = maxdate + relativedelta(months=-12)
     results = session.query(Measurement.date, Measurement.tobs).filter(and_(Measurement.date>=last_12_months, Measurement.date <= maxdate)).all()
-    # results = session.query(Measurement.date, Measurement.tobs).limit(10).all()
     session.close()
     all_measurements = []
     # format into JSON
@@ -87,11 +99,59 @@ def tobs(type):
         measurement_dict['Precipitation'] = tobs
         all_measurements.append(measurement_dict)
     
-    if type != "raw":
+    if type == "inline":
         return render_template('index.html', json_content=all_measurements, message_type="Success", message_content="Success! Returning the last 12 months Total Observed Temperature data")
-    else:
+    elif type == "raw":
         return jsonify(all_measurements)
+    else:
+        abort(404)
 
+@app.route("/api/v1.0/<type>/<start>", methods=['GET', 'POST'])
+def stats_start(type, start):
+    session = Session(engine)
+    try:
+        start_date = dt.datetime.strptime(str(start), "%Y%m%d")
+    except:
+        abort(400)
+    results = session.query(func.min(Measurement.tobs), func.avg(Measurement.tobs), func.max(Measurement.tobs)).filter(Measurement.date>=start_date).all()
+    session.close()
+    all_measurements = {
+                'Start Date' : start_date.strftime("%Y-%m-%d"),
+                'Minimum Temperature' : results[0][0],
+                'Average Temperature' : results[0][1],
+                'Maximum Temperature' : results[0][2]
+        }
+    if type == "inline":
+        return render_template('index.html', json_content=all_measurements, message_type="Success", message_content=f"Success! Returning the Temperature Observations since {start_date.strftime('%Y-%m-%d')}")
+    elif type == "raw":
+        return jsonify(all_measurements)
+    else:
+        abort(404)
+
+
+@app.route("/api/v1.0/<type>/<start>/<end>", methods=['GET', 'POST'])
+def stats_start_end(type, start, end):
+    session = Session(engine)
+    try:
+        start_date = dt.datetime.strptime(str(start), "%Y%m%d")
+        end_date = dt.datetime.strptime(str(end), "%Y%m%d")
+    except:
+        abort(400)
+    results = session.query(func.min(Measurement.tobs), func.avg(Measurement.tobs), func.max(Measurement.tobs)).filter(and_(Measurement.date>=start_date, Measurement.date<=end_date)).all()
+    session.close()
+    all_measurements = {
+                'Start Date' : start_date.strftime("%Y-%m-%d"),
+                'End Date' : end_date.strftime("%Y-%m-%d"),
+                'Minimum Temperature' : results[0][0],
+                'Average Temperature' : results[0][1],
+                'Maximum Temperature' : results[0][2]
+        }
+    if type == "inline":
+        return render_template('index.html', json_content=all_measurements, message_type="Success", message_content=f"Success! Returning the Temperature Observations between the dates {start_date.strftime('%Y-%m-%d')} and {end_date.strftime('%Y-%m-%d')}")
+    elif type == "raw":
+        return jsonify(all_measurements)
+    else:
+        abort(404)
 
 if __name__ == "__main__":
     app.run(debug=True)
